096558cc432fa968e126ab6f8d3fec640b8fb581
Nattanicha11 <6811860012@mut.ac.th>
2025-11-23 17:26:13 +0700

    การทดลองครั้งที่2

---
+++ LAB02/TEST01/TEST01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << " Hello Word!" << std::endl;
+    std::cout << " Hello LAB2!" << std::endl;
+
+    std::cout << "Name: Nattanicha Chatmontri" << std::endl;
+    std::cout << "Course: MIIA0106 Python and C Programming Langguage" << std::endl;
+    std::cout << "Email: 6811860012@mut.ac.th" << std::endl;
+
+    return 0;
+}
